915b74f6e0b54dbee92a0d76987da9696facf92a
SenthurAthiban94 <senthurathiban94@gmail.com>
2016-06-02 01:09:12 +0530

    contacts app

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -16,12 +33,42 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+            app.receivedEvent('deviceready');            
+                   var options = new ContactFindOptions();
+                   options.filter="";
+                   options.filter="";
+                   options.multiple=true;
+                   var fields = ["*"];  //"*" will return all contact fields
+                   navigator.contacts.find(fields, app.onSuccess, app.onError, options);
+                
+    },
+    onSuccess : function(contacts){
+         //console.log(JSON.stringify(contacts))
+                   var li = '';
+                   $.each(contacts, function(key, value) {
+                        if(value.name){
+                            $.each(value.name, function(key, value) {
+                               if(key == 'formatted'){
+                                   name = value;
+                               }                      
+                            });
+                        }
+                        if(value.phoneNumbers){
+                            $.each(value.phoneNumbers, function(key, value) {
+                                phone = value.value;
+                            });
+                        }                    
+                        li += '<li style="text-decoration:none;">'+name+' '+phone+'</li>';
+                   }); 
+                
+                   $("#contact").html(li);
+    },
+    onError   : function(contactError){
+             alert("Some Error Occured");
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        console.log('Received Event: ' + id);
     }
 };
+
 app.initialize();
