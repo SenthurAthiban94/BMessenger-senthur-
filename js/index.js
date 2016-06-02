f79a17613d71c0c7d091cb13b9cadf20ed79463d
SenthurAthiban94 <senthurathiban94@gmail.com>
2016-06-02 23:00:46 +0530

    i think loading bug fixed

--- js/index.js
+++ js/index.js
@@ -41,8 +41,9 @@ var app = {
          /* Retriving phoneNumbers */
          for(var i=0, len=c.length; i<len; i++) {
              if(c[i].phoneNumbers && c[i].phoneNumbers.length > 0) {
-                tablebody.innerHTML += "<tr><td class=\"col-sm-1 col-xs-1 col-md-1\">"+(count+1)+"</td><td class=\"col-sm-9 col-xs-9 col-md-9\" style=\"font-size:16px;\"><div><div><b><span>Name</span>: </b><span style=\"padding-left:2px;\">"+c[i].displayName+"</span></div></div><div><div><b><span>Number</span>: </b><span style=\"padding-left:2px;\">"+c[i].phoneNumbers[0].value+"</span></div></div></td><td class=\"col-sm-2 col-xs-2 col-md-2\"style=\"padding-top:10px;padding-bottom:10px;\"><button type=\"button\" class=\"btn btn-warning\" style=\"height:35px;padding-top:2px;padding-bottom:2px;\">Upload</button></td></tr>";
+                tablebody.innerHTML += "<tr><td class=\"col-sm-1 col-xs-1 col-md-1\">"+(count=count+1)+"</td><td class=\"col-sm-9 col-xs-9 col-md-9\" style=\"font-size:16px;\"><div><div><b><span>Name</span>: </b><span style=\"padding-left:2px;\">"+c[i].displayName+"</span></div></div><div><div><b><span>Number</span>: </b><span style=\"padding-left:2px;\">"+c[i].phoneNumbers[0].value+"</span></div></div></td><td class=\"col-sm-2 col-xs-2 col-md-2\"style=\"padding-top:10px;padding-bottom:10px;\"><button type=\"button\" class=\"btn btn-warning\" style=\"height:35px;padding-top:2px;padding-bottom:2px;\">Upload</button></td></tr>";
              }
+             $('#loaderbackground').show();
          }
          $('#loaderbackground').hide();
         
